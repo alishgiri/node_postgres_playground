541dbfe0e785f5b46f9dc7d0dbeb9e44346ee35a
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2021-06-19 17:13:22 +0545

    Setup node server & create basic route

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-const express = required("express");
+const express = require("express");
 
+const User = require("./db/models/user");
 const dbConfig = require("./db/db_config");
 
 const app = express();
 app.use(express.json());
 
+const { PORT } = process.env;
+
+app.get("/user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.query().findById(id);
+    res.send(user);
+  } catch (e) {
+    res.status(400).send({ message: "Something went wrong!", e });
+  }
+});
+
 dbConfig();
+
+app.listen(PORT, () => {
+  console.log("Listening on port:", PORT);
+});
