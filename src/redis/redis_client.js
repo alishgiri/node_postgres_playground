f4c9e43c90f6212c635e0b7f54ef72235222398c
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2021-07-19 22:29:57 +0545

    Comment out code causing issue with chat app

--- src/redis/redis_client.js
+++ src/redis/redis_client.js
@@ -4,15 +4,15 @@ const config = require("./config");
 const client = redis.createClient(config.redis_port, config.redis_host);
 const publishClient = redis.createClient(config.redis_port, config.redis_host);
 
-client.set("REDIS_KEY", "0");
-client.subscribe("REQUESTS");
+// client.set("REDIS_KEY", "0");
+// client.subscribe("REQUESTS");
 
-client.on("error", (err) => {
-  console.log("REDIS ERROR", err);
-});
+// client.on("error", (err) => {
+//   console.log("REDIS ERROR", err);
+// });
 
-client.on("message", (channel, message) => {
-  console.log("REDIS message", { channel, message });
-});
+// client.on("message", (channel, message) => {
+//   console.log("REDIS message", { channel, message });
+// });
 
 module.exports = { client, publishClient };
