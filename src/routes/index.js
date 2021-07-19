2209c99d0e300f01add5bad812bae03998ee6c01
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2021-07-19 20:10:24 +0545

    use /redis for redis routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const users = require("./users.route");
 const restaurants = require("./restaurants.route");
 
 module.exports.initRoutes = (app) => {
-  app.use("/", redis);
+  app.use("/redis", redis);
   app.use("/api/v1/users", users);
   app.use("/api/v1/restaurants", restaurants);
 };
