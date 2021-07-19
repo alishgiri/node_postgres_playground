39bed0a6d90c3933cb2ed5b8ac83b688fe38e462
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2021-07-19 20:00:29 +0545

    Add publishClient as well to redis client file

--- src/redis/redis_client.js
+++ src/redis/redis_client.js
@@ -1,11 +1,18 @@
 const redis = require("redis");
 const config = require("./config");
 
-const redisClient = redis.createClient(config.redis_port, config.redis_host);
-redisClient.set("REDIS_KEY", "0");
+const client = redis.createClient(config.redis_port, config.redis_host);
+const publishClient = redis.createClient(config.redis_port, config.redis_host);
 
-redisClient.on("error", (err) => {
+client.set("REDIS_KEY", "0");
+client.subscribe("REQUESTS");
+
+client.on("error", (err) => {
   console.log("REDIS ERROR", err);
 });
 
-module.exports = redisClient;
+client.on("message", (channel, message) => {
+  console.log("REDIS message", { channel, message });
+});
+
+module.exports = { client, publishClient };
