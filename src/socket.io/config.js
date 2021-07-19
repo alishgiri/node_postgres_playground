b0198f86236a93c96090b8c469a2f5ef57823a2b
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2021-07-19 22:29:05 +0545

    Update socketio configuration for chat app

--- src/socket.io/config.js
+++ src/socket.io/config.js
@@ -1,14 +1,37 @@
 const socketio = require("socket.io");
+const { storeUser, getUser } = require("../services/redis.service");
 
-const initSocketIO = (server) => {
+const errorEmit = (socket) => {
+  return (err) => {
+    console.log(err);
+    socket.broadcast.emit("user.events", "Something went wrong!");
+  };
+};
+
+module.exports.initSocketIO = (server) => {
   const io = socketio(server);
 
   io.on("connection", (socket) => {
-  // Using broadcast everyone but not self gets the message
+    // Using broadcast everyone but not self gets the message
     socket.broadcast.emit("user.events", "Someone has joined!");
+
     socket.on("name", (name) => {
-      console.log(name + " says hello!");
-      socket.broadcast.emit("name", name);
+      storeUser(socket.id, name).then(() => {
+        console.log(`${name} says hello!`);
+        socket.broadcast.emit("name", name);
+      }, errorEmit(socket));
+    });
+
+    socket.on("disconnect", () => {
+      getUser(socket.id)
+        .then((user) => {
+          if (!user) return "Unknown";
+          return user;
+        })
+        .then((user) => {
+          console.log(`${user} left`);
+          socket.broadcast.emit("user.events", `${user} left`);
+        }, errorEmit(socket));
     });
   });
 
@@ -20,5 +43,3 @@ const initSocketIO = (server) => {
   //   });
   // });
 };
-
-module.exports = initSocketIO;
