fb749e20456cf5cffe39bacbbf489fc5dc634309
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2021-07-21 10:14:54 +0545

    Register seperate route for /socketio

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const redis = require("./redis.route");
 const users = require("./users.route");
+const socketio = require("./pages.route");
 const restaurants = require("./restaurants.route");
 
 module.exports.initRoutes = (app) => {
   app.use("/redis", redis);
+  app.use("/socketio", socketio);
   app.use("/api/v1/users", users);
   app.use("/api/v1/restaurants", restaurants);
 };
